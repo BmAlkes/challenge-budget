9a0414bcb3944d73115bcc26b7eea9880bb370ab
bmalkes <bmalkes@gmail.com>
2022-06-23 07:08:51 +0300

    push deploy

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://challenge-budget-app-backend.herokuapp.com",
+    baseURL: "https://challenge-budget-backend-app.herokuapp.com",
 });
 
 export const createSession = async (email, password) => {
